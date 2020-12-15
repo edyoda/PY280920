97deb01e205b21ed189784205e2d95c2af6f263c
Mohit Singh <mohitchouhan1947@gmail.com>
2020-12-15 21:28:47 +0530

    w3d2

--- DBMS/W03D02/company_DB_2.sql
+++ DBMS/W03D02/company_DB_2.sql
@@ -1,11 +1,11 @@
 -- BRANCH SUPPLIER
--- INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
--- INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
--- INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
--- INSERT INTO branch_supplier VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
--- INSERT INTO branch_supplier VALUES(3, 'Uni-ball', 'Writing Utensils');
--- INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
--- INSERT INTO branch_supplier VALUES(3, 'Stamford Lables', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(3, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier VALUES(3, 'Stamford Lables', 'Custom Forms');
 
 -- CLIENT
 INSERT INTO client VALUES(400, 2, 'Dunmore Highschool');
